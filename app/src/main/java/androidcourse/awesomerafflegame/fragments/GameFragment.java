a4fba1ace6b82bae37d254fc28a2592cb1cc5d96
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-05-01 19:53:14 +0200

    Game fragment

---
+++ app/src/main/java/androidcourse/awesomerafflegame/fragments/GameFragment.java
@@ -0,0 +1,23 @@
+package androidcourse.awesomerafflegame.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidcourse.awesomerafflegame.R;
+
+/**
+ * Created by Jesper on 01/05/16.
+ */
+public class GameFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_game, container, false);
+        return view;
+    }
+}
