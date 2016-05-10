df6831fc6a128b7b777e260af67d504d73ea69c5
Mads Justesen <mads@medware.dk>
2016-05-10 20:12:21 +0200

    ...

--- app/src/main/java/androidcourse/awesomerafflegame/fragments/PreGameFragment.java
+++ app/src/main/java/androidcourse/awesomerafflegame/fragments/PreGameFragment.java
@@ -2,7 +2,6 @@ package androidcourse.awesomerafflegame.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,13 +30,12 @@ public class PreGameFragment extends Fragment implements View.OnClickListener {
     private LinearLayout blueLayout;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_bluetooth, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         btnVsComputer = (Button) view.findViewById(R.id.btnVsComputer);
         btnVsComputer.setOnClickListener(this);
         btnVsPlayer = (Button) view.findViewById(R.id.btnVsPlayer);
